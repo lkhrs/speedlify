22f5b97363e3f5d624133bb3867cb848547f09b5
Luke <luke+git@lkhrs.com>
2022-09-19 10:21:19 -0500

    [sites] add my list

---
+++ _data/sites/lkhrs.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: "Lukes Wild Websites", // optional, falls back to object key
+	description: "a list of Luke's websites",
+	options: {
+		frequency: 60 * 23, // (in minutes), 23 hours
+	},
+	urls: [
+		"https://www.lkhrs.com/",
+		"https://www.webonade.com/",
+		"https://lukeloveslauren.com/"
+	]
+};
